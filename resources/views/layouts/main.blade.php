0c7a0fc4f05cbe072984dd66f9025038a563f4e0
jose1193 <josegonzalezcr2794@gmail.com>
2025-03-28 22:48:54 +0000

    FIX: Portfolio

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>@yield('title') - V General Contractors</title>
-
+    <x-facebook-pixel />
     <!-- Styles and Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
