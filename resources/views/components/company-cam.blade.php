e8dbbb3a415317ad830e386e6f62a2e8544ab0bb
jose1193 <josegonzalezcr2794@gmail.com>
2025-03-31 17:04:47 +0000

    FIX: Services

--- resources/views/components/company-cam.blade.php
+++ resources/views/components/company-cam.blade.php
@@ -1,10 +1,10 @@
 @props(['showcaseId' => null])
 <section class="py-16 bg-white fade-in-section">
     <div class="container mx-auto px-4">
-        <div class="company-cam-container my-8">
+        <div class="company-cam-container my-8 overflow-hidden">
 
             <div data-showcase-id="{{ $showcaseId ?? config('services.companycam.showcase_id', '01057770-8ca0-47a5-a1dc-40128a20f85b') }}"
-                id="companycam-showcase-root"></div>
+                id="companycam-showcase-root" style="margin-top: -45px;"></div>
         </div>
     </div>
 </section>
