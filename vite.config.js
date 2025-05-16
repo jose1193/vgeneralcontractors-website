b01d930a71591c17561ec79926b10ea847d56760
jose1193 <josegonzalezcr2794@gmail.com>
2025-05-16 15:25:32 +0000

    FIX: Htaccess

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,10 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    server: {
-        // Activamos HTTPS ya que tenemos certificado SSL
-        https: true,
-        host: "0.0.0.0",
-        hmr: {
-            host: "localhost",
-        },
-    },
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
-            // Añadir configuración para producción
-            publicDirectory: "public",
         }),
     ],
 });
