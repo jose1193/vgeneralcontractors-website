310af055e455f0c282caea4f2ad4ca2bdc616047
jose1193 <josegonzalezcr2794@gmail.com>
2025-05-21 21:36:43 +0000

    ADD: Retell Api key

--- resources/views/livewire/call-records.blade.php
+++ resources/views/livewire/call-records.blade.php
@@ -108,7 +108,7 @@
                                             class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full 
                                             @if ($sentiment === 'Positive') bg-green-100 text-green-800
                                             @elseif($sentiment === 'Negative')
-                                                 
+                                                bg-red-100 text-red-800
                                             @else
                                                 bg-gray-100 text-gray-800 @endif">
                                             {{ $sentiment ?? 'Unknown' }}
