5eec1c0638003870dc0dec932401657aa08f7ffd
jose1193 <josegonzalezcr2794@gmail.com>
2025-05-22 23:59:22 +0000

    FIX: Permission Storage

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,13 +2,13 @@
 set -e
 
 # Ensure proper permissions
-sudo chown -R www-data:www-data /var/www/html/storage
-sudo chmod -R 775 /var/www/html/storage
+#sudo chown -R www-data:www-data /var/www/html/storage
+#sudo chmod -R 775 /var/www/html/storage
 
 # Ensure cron log directory exists and has proper permissions
-mkdir -p /var/www/html/storage/logs
-sudo chown -R www-data:www-data /var/www/html/storage/logs
-sudo chmod -R 775 /var/www/html/storage/logs
+#mkdir -p /var/www/html/storage/logs
+#sudo chown -R www-data:www-data /var/www/html/storage/logs
+#sudo chmod -R 775 /var/www/html/storage/logs
 
 # Remove the PID file if it exists
 rm -f /var/run/crond.pid
