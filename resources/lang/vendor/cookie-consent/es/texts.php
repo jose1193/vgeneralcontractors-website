2dcf9008eff4ddc065f6e83019c64660bbce8dad
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-11 16:42:10 +0000

    FIX: multilenguage

---
+++ resources/lang/vendor/cookie-consent/es/texts.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'message' => 'Usamos cookies para mejorar tu experiencia en nuestro sitio web. Al continuar navegando, aceptas el uso de cookies para fines de análisis, personalización y publicidad. Tus datos nos ayudan a ofrecer mejores servicios de techado.',
+    'agree' => 'Aceptar Cookies',
+    'dismiss' => 'Rechazar',
+    'link' => 'Más Información',
+]; 
