638e963e980d3822597af9c2498f61048d396741
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-13 23:30:43 +0000

    FIX: theme dark

--- resources/views/components/livewire/blog-categories/data-table.blade.php
+++ resources/views/components/livewire/blog-categories/data-table.blade.php
@@ -1,4 +1,4 @@
-<div class="overflow-x-auto">
+<div class="overflow-x-auto bg-white dark:bg-gray-800 shadow-md rounded-lg">
     <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
         <thead class="bg-gray-50 dark:bg-gray-800">
             <tr>
