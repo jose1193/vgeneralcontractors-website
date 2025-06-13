e4e2f20bb5e49ddd39c6c3b5a350c3d576eebe2a
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-13 23:17:14 +0000

    FIX: theme dark

--- resources/views/blog-categories.blade.php
+++ resources/views/blog-categories.blade.php
@@ -1,16 +1,14 @@
 <x-app-layout>
-    <x-slot name="header">
-        <div class="bg-gray-800" style="background-color: #141414;">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <h2 class="text-2xl font-semibold leading-tight text-white">
-                    {{ __('blog_categories_title') }}
-                </h2>
-                <p class="mt-2 text-sm text-gray-300">
-                    {{ __('blog_categories_subtitle') }}
-                </p>
-            </div>
+    <div class="bg-gray-800" style="background-color: #141414;">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h2 class="text-2xl font-semibold leading-tight text-white">
+                {{ __('blog_categories_title') }}
+            </h2>
+            <p class="mt-2 text-sm text-gray-300">
+                {{ __('blog_categories_subtitle') }}
+            </p>
         </div>
-    </x-slot>
+    </div>
 
     @livewire('blog-categories')
 </x-app-layout>
