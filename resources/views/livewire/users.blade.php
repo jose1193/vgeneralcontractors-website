015967990d91ba3d105182352975cecbdffe9168
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-14 00:53:25 +0000

    FIX: multilenguage

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto py-10">
         <!-- Success and error messages -->
         @if (session()->has('message'))
             <x-alerts.success :message="session('message')" />
@@ -37,14 +37,12 @@
                     </div>
                 </div>
 
-                <!-- Users table with extra spacing on mobile -->
-                <div class="mt-8 sm:mt-6">
-                    @include('components.livewire.users.data-table', [
-                        'users' => $users,
-                        'sortField' => $sortField,
-                        'sortDirection' => $sortDirection,
-                    ])
-                </div>
+                <!-- Users table -->
+                @include('components.livewire.users.data-table', [
+                    'users' => $users,
+                    'sortField' => $sortField,
+                    'sortDirection' => $sortDirection,
+                ])
 
                 <!-- Pagination -->
                 <x-pagination :paginator="$users" />
