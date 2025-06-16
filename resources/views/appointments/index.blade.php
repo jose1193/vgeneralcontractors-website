69ba2892f01152230afb2251932c2dca7210dccc
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-16 23:46:06 +0000

    FIX: livewire

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -425,7 +425,7 @@
 
                                     let actionsHtml = `
                                 <div class="flex justify-center space-x-1">
-                                    <a href="${editUrl}" class="btn btn-icon btn-sm btn-primary">
+                                    <a href="${editUrl}" class="inline-flex items-center justify-center p-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors duration-200" title="Edit">
                                          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                          </svg>
