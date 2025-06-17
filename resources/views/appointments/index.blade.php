140f43b0709781952922392b1ef0614f3fab26fd
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-17 00:08:11 +0000

    FIX: livewire

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -1037,7 +1037,7 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"/>
                                     </svg>
                                     <span class="text-sm font-medium">{{ __('copy_link') }}</span>
-                                </button>
+                            </button>
                             </div>
                             
                             <div class="text-center text-sm text-gray-500 dark:text-gray-400">
