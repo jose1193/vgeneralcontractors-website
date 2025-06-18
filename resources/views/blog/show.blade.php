845f7164468097f52a5a3696de3dd7d5807dbf82
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-18 02:22:49 +0000

    FIX: Crud Posts

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -126,7 +126,7 @@
                     <!-- Post content -->
                     <article
                         class="prose prose-lg max-w-none prose-headings:text-gray-900 prose-headings:font-bold prose-p:text-gray-700 prose-img:rounded-lg">
-                        {!! str_replace(['<br>', '<br/>', '<br />'], '', $post->post_content) !!}
+                        {!! $post->post_content !!}
                     </article>
 
                     <!-- Tags -->
