10022c0590536c8e93cf38c8a43bf9bb2ea42b0c
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-25 23:32:55 +0000

    FIX: Calendar Blade

--- public/js/calendar-modals.js
+++ public/js/calendar-modals.js
@@ -1384,7 +1384,6 @@ class CalendarModals {
         if (phoneInput) {
             phoneInput.addEventListener("input", (event) => {
                 formatPhoneInput(phoneInput, event);
-                debouncedPhoneValidation(event.target);
                 checkFormValidity();
             });
             phoneInput.addEventListener("blur", (event) => {
