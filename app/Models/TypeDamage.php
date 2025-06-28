1b1ec3e528eaefd12c8e961bf6245205f299f45d
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-28 22:20:42 +0000

    FIX: Invoice Demo Blade

--- app/Models/TypeDamage.php
+++ app/Models/TypeDamage.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TypeDamage extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
       protected $fillable = [
         'uuid',
         'type_damage_name',
