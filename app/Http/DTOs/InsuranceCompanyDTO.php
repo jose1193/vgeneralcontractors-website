6c1ba5f6f7db69cc6f37a716b06757e2837e80d4
jose1193 <josegonzalezcr2794@gmail.com>
2025-06-29 00:16:57 +0000

    FIX: Insurance company service

--- app/Http/DTOs/InsuranceCompanyDTO.php
+++ app/Http/DTOs/InsuranceCompanyDTO.php
@@ -6,11 +6,11 @@ class InsuranceCompanyDTO extends BaseDTO
 {
     public ?string $uuid = null;
     public string $insurance_company_name;
-    public string $address;
-    public string $phone;
-    public string $email;
+    public ?string $address = null;
+    public ?string $phone = null;
+    public ?string $email = null;
     public ?string $website = null;
     public ?int $user_id = null;
 
     // Puedes agregar métodos de validación, transformación, etc. si lo necesitas
-} 
+}
