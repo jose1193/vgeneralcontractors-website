4b4d61df15abdda200701091b648105bc5ead27e
jose1193 <josegonzalezcr2794@gmail.com>
2025-07-02 14:41:35 +0000

    FIX: Invoice Demo

--- resources/views/invoice-demos/index.blade.php
+++ resources/views/invoice-demos/index.blade.php
@@ -818,16 +818,6 @@
                                                     :class="invoice.deleted_at ? 'text-red-200 line-through' : 'text-gray-100'"
                                                     x-text="invoice.invoice_number">
                                                 </div>
-                                                <div x-show="invoice.deleted_at" class="ml-2 text-red-400"
-                                                    title="Deleted">
-                                                    <svg class="w-4 h-4" fill="none" stroke="currentColor"
-                                                        viewBox="0 0 24 24">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
-                                                        </path>
-                                                    </svg>
-                                                </div>
                                                 <div x-show="invoice.pdf_url" class="ml-2 text-green-400"
                                                     title="PDF Available">
                                                     <svg class="w-4 h-4" fill="none" stroke="currentColor"
