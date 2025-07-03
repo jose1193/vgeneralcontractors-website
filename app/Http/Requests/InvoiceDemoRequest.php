68af14609f5690a87f41dc73af0de423d417e02b
jose1193 <josegonzalezcr2794@gmail.com>
2025-07-03 17:15:38 +0000

    FIX: Invoice Demo

--- app/Http/Requests/InvoiceDemoRequest.php
+++ app/Http/Requests/InvoiceDemoRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Log;
-
+use Illuminate\Validation\Rule;
 class InvoiceDemoRequest extends FormRequest
 {
     /**
