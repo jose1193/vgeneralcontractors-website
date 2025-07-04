1072d146e03fd36ddcc7b1b787a63330fe3b0d68
jose1193 <josegonzalezcr2794@gmail.com>
2025-07-04 13:20:53 +0000

    FIX: Feed RSS

--- resources/views/invoice-demos/partials/invoice-modal.blade.php
+++ resources/views/invoice-demos/partials/invoice-modal.blade.php
@@ -369,7 +369,7 @@
                                         <div>
                                             <label class="block text-sm font-medium text-gray-700 mb-1">Rate</label>
                                             <input type="text" x-model="item.rate"
-                                                x-mask="99999.99"
+                                                x-mask:dynamic="$money($input)"
                                                 @input="calculateTotals()"
                                                 class="w-full rounded-lg border-gray-300 shadow-sm text-sm"
                                                 placeholder="0.00">
