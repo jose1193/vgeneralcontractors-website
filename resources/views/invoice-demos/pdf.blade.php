98c83ae32a6dba2ac8ad0bc2ef7640e68ebd5c4f
jose1193 <josegonzalezcr2794@gmail.com>
2025-07-10 16:36:49 +0000

    FIX: Invoice Demo

--- resources/views/invoice-demos/pdf.blade.php
+++ resources/views/invoice-demos/pdf.blade.php
@@ -31,7 +31,7 @@
             width: 50%;
             text-align: left;
             vertical-align: top;
-            padding-left: 117px;
+            padding-left: 120px;
             padding-top: 23px;
         }
         .logo {
