80d3cc327590f0c356c7e446846a5494c7195a6f
jose1193 <josegonzalezcr2794@gmail.com>
2025-07-16 15:34:50 +0100

    FIX: Glassmorphic Table

--- resources/views/components/crud/toggle-show-deleted.blade.php
+++ resources/views/components/crud/toggle-show-deleted.blade.php
@@ -5,7 +5,7 @@
     'class' => '',
 ])
 
-<div class="flex items-center {{ $class }}">
+<div class="flex items-center justify-end sm:justify-start w-full sm:w-auto {{ $class }}">
     <label for="{{ $id }}" class="mr-2 text-sm font-medium text-white/90 capitalize">
         📄 {{ $label }}
     </label>
