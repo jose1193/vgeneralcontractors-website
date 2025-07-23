a3642d7969b22908b1100837ad10edc9f790150e
jose1193 <josegonzalezcr2794@gmail.com>
2025-07-23 15:06:10 +0100

    FIX: Insurance Export

--- app/Helpers/CompanyDataHelper.php
+++ app/Helpers/CompanyDataHelper.php
@@ -20,7 +20,7 @@ class CompanyDataHelper
                 'phone' => self::formatPhoneForDisplay($companyData->phone) ?: '+1 (713) 364-6240',
                 'email' => $companyData->email ?: 'info@vgeneralcontractors.com',
                 'website' => $companyData->website ?: 'https://vgeneralcontractors.com/',
-                'logo_path' => public_path('assets/logo/header-document.png'),
+                'logo_path' => public_path('assets/logo/header-document.jpg'),
                 'signature_path' => $companyData->signature_path,
                 'latitude' => $companyData->latitude,
                 'longitude' => $companyData->longitude,
