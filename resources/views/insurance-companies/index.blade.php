a287ca574834ae850d3bbe72bad4a6dce83b3246
jose1193 <josegonzalezcr2794@gmail.com>
2025-07-26 02:08:45 +0100

    FIX: Translate

--- resources/views/insurance-companies/index.blade.php
+++ resources/views/insurance-companies/index.blade.php
@@ -71,7 +71,7 @@
                     // Configuración de filtros de fecha
                     dateField: 'created_at', // Campo de fecha por defecto
                     // Configuración de numeración secuencial
-                    showSequentialNumbers: false, // Deshabilitado porque ya está en table-columns
+                    showSequentialNumbers: true, // Deshabilitado porque ya está en table-columns
                     sequentialNumberLabel: "{{ __('nro') }}",
                     entityConfig: {
                         identifierField: 'insurance_company_name',
