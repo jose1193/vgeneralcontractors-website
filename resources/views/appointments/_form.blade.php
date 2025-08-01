d6bcdc0df601465ac0d1da9e5667e41df0aba697
jose1193 <josegonzalezcr2794@gmail.com>
2025-08-01 15:23:03 +0100

    FIX: Insurance PDF

--- resources/views/appointments/_form.blade.php
+++ resources/views/appointments/_form.blade.php
@@ -432,7 +432,8 @@
             <label class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2 md:text-right">
                 {{ __('Additional Options') }}
             </label>
-            <div class="flex flex-col md:flex-row md:justify-end md:items-center space-y-2 md:space-y-0 md:space-x-6 mt-2">
+            <div
+                class="flex flex-col md:flex-row md:justify-end md:items-center space-y-2 md:space-y-0 md:space-x-6 mt-2">
                 <label for="sms_consent" class="inline-flex items-center cursor-pointer">
                     <input id="sms_consent" name="sms_consent" type="checkbox" value="1"
                         class="checkbox-field form-checkbox text-yellow-500 h-5 w-5 border-gray-300 rounded focus:ring-yellow-500"
